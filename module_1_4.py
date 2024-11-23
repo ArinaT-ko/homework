a0f9c3eec833d58fbe8647abd478a4968b84172a
ArinaT-ko <1.618.1.618g@gmail.com>
2024-11-23 21:21:40 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,9 @@
+made_dz = 12
+
+hours =  1.5
+
+course_name =  'Python'
+
+one_time = made_dz/hours
+
+print('Курс: ', course_name, ';', ' всего задач: ', made_dz, ';', ' затрачено часов: ', hours, ';', ' среднее время выполнения: ', one_time, ' часа.', sep = '')
